8edd4c331ae4f187ed13fbaae9e08ac618b34084
lizheng <v-lizheng@expediagroup.com>
2022-06-10 15:21:09 +0800

    add footer

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <main>
+            <Container>
+                <p>1222</p>
+            </Container>
+            </main>
+            <Footer></Footer>
+        </div>
+    )
+}
