abb1d853e9399f6cd2febaa2614866e0bb2b1da9
lizheng <v-lizheng@expediagroup.com>
2022-06-14 23:15:32 +0800

    add register

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -38,16 +38,16 @@ export const register = (email, password) => async(dispatch, getState) => {
         const { data: { data, success } } = await axios.post('/api/users/register', { email, password }, config);
         console.log(data, success)
         if (success) {
-            dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+            dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
             localStorage.setItem('userInfo', JSON.stringify(data))
         } else {
-            dispatch({ type: USER_LOGIN_FAIL })
+            dispatch({ type: USER_REGISTER_FAIL })
         }
 
     } catch (error) {
 
         dispatch({
-            type: USER_LOGIN_FAIL,
+            type: USER_REGISTER_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message : error.message
         })
     }
--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -14,12 +14,15 @@ export default function RegisterScreen() {
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     const [searchParams, setSearchParams] = useSearchParams();
+    const [error,setError]=useState('')
     const redirect = searchParams.get("redirect") || '/';
     const dispatch = useDispatch();
     const userLogin = useSelector((state) => state.userLogin);
-    const { loading, error, userInfo } = userLogin;
+    const { loading, error:oldError, userInfo } = userLogin;
     const navigate = useNavigate();
+    setError(oldError)
 
     useEffect(() => {
         if (userInfo) {
@@ -29,14 +32,18 @@ export default function RegisterScreen() {
 
 
     const submitHandler = (e) => {
-        e.preventDefault()
-        dispatch(login(email, password));
+        e.preventDefault();
+        if(password!==confirmPassword){
+            setError('password not match')
+            return;
+        }
+        dispatch(register(email, password,name));
     }
 
     return (
         <FormContainer>
-            <h1>Sign In</h1>
-            {error && <Message variant="danger">{error}</Message>}
+            <h1>Sign up</h1>
+            {error && <Message variant='danger'>{error}</Message>}
             {loading && <Loader />}
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId='email'>
@@ -67,15 +74,24 @@ export default function RegisterScreen() {
                         onChange={(e) => setPassword(e.target.value)}
                     ></Form.Control>
                 </Form.Group>
+                <Form.Group controlId='confirmPassword'>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Enter password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
                 <Row className='py-3'>
 
-                    <Button type="submit" variant="primary">Sign In</Button>
+                    <Button type="submit" variant="primary">Sign Up</Button>
                 </Row>
 
                 <Row className="py-3">
                     <Col>
                         New Customer?{' '}
-                        <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
+                        <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>login</Link>
                     </Col>
                 </Row>
             </Form>
