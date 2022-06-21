06b469804b699b33b327441a62706259ff1e73ab
lizheng <v-lizheng@expediagroup.com>
2022-06-21 21:08:38 +0800

    fix delete product permit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { getProducts, getProduct, deleteProduct } from "../contoller/productConroller.js";
-
+import { auth, admin } from '../middleware/auth.middleware.js'
 
 const router = express.Router();
 
 
 router.get('/', getProducts);
 router.get('/:id', getProduct);
-router.delete('/:id', deleteProduct);
+router.delete('/:id', auth, admin, deleteProduct);
 
 
 export default router;
